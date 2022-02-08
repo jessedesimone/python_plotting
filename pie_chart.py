2993dea3bf0518e9426ac51df34851d6f8165554
jessedesimone <desimone.neuro@gmail.com>
2022-02-08 13:10:12 -0500

    initial commit

---
+++ pie_chart.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3.9
+
+'''
+Change Log
+=========
+0.0.1 (2022-02-08)
+Initial Commit
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set Parameters
+fontdict= {'fontsize': 14, 'fontweight': 'bold'}
+bbox={'facecolor':'0.8', 'pad':5}
+colors = ['yellow', 'cyan', 'blue', 'red', 'teal']
+plt.style.use('fivethirtyeight')
+
+# Define Labels and Sizes
+labels = ['Large Cap', 'Mid Cap', 'Small Cap', 'Bond', 'Choice']
+sizes = np.array([55, 25, 10, 7, 3])
+
+# Plot Pie Chart Option 1
+fig, ax1 = plt.subplots(figsize=(5,5))
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90)
+ax1.set_title('Investment Weight Targets', fontdict=fontdict, bbox=bbox)
+plt.tight_layout()
+
+# Plot Pie Chart Option 2
+plt.pie(sizes, labels=labels)
+plt.title('Investment Weight Targets', fontdict=fontdict, bbox=bbox)
+plt.legend()
+plt.tight_layout()
+plt.show()
