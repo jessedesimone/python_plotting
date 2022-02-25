c60011e44d1630c2e1a8b9a1281535fec2559eb8
jessedesimone <desimone.neuro@gmail.com>
2022-02-25 09:07:16 -0500

    2.26 update

--- pie_chart.py
+++ pie_chart.py
@@ -17,13 +17,13 @@ bbox={'facecolor':'0.8', 'pad':5}
 plt.style.use('fivethirtyeight')
 
 # Define Labels and Sizes
-labels = ['HHS','DOD','DOE','NSF','NASA','DHS','USDA','DOT','DOC','ED','EPA']
-sizes = np.array([1.02,1.571,0.267,0.188,0.162,0.021,0.021,0.011,0.010,0.010,0.005])
+labels = ['Large Cap', 'Mid Cap', 'Small Cap', 'Bond']
+sizes = np.array([60, 20, 10, 10])
 
 # Plot Pie Chart Option 1
 fig, ax1 = plt.subplots(figsize=(5,5))
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90)
 ax1.set_title('Investment Weight Targets', fontdict=fontdict, bbox=bbox)
 plt.tight_layout()
-plt.legend()
+#plt.legend()
 plt.show()
