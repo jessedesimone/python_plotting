558a4dfa34dc749d7ff79fb5d12f6ce034130962
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-08-14 21:38:58 -0400

    230814

--- lmplot.py
+++ lmplot.py
@@ -21,3 +21,25 @@ plt.xlabel('')
 plt.ylabel('')
 plt.tight_layout()
 plt.show()
+
+#--------use desimone.mplstype-------
+
+import os
+import matplotlib.pyplot as plt
+os.chdir('/set/root/directory')
+df=pd.read_csv('input_file.csv')
+sns.set_context("talk")
+plt.style.use('desimone.mplstyle')
+x=''
+y=''
+
+my_cols = {'grp1': 'cyan', 'grp2': 'steelblue', 'grp3': 'lightslategrey'}
+g = sns.lmplot(x=x, y=y, hue='grp', data=df, fit_reg=False, legend=False,
+               palette=my_cols, scatter_kws={"s": 100, 'linewidths':1,'edgecolor':'black'})
+g=sns.regplot(x=x, y=y, data=df, scatter=False, ax=g.axes[0, 0], line_kws={"color": "black"})
+plt.axvline(x=0.160, color='black', linestyle = 'dashed')       #plot vertical line
+plt.xlabel('')
+plt.ylabel('')
+plt.tight_layout()
+plt.savefig('out_file.jpg', dpi=300, bbox_inches='tight')
+plt.show()
